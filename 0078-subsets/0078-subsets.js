07cd0e094c0b5b9857fa29cef5f43376f8bfad66
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-07-17 22:58:45 -0500

    Time: 68 ms (36.48%), Space: 42.6 MB (92.30%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -3,16 +3,15 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    const subsetCount = Math.pow(2, nums.length);
-    results = [];
-    for (let i = 0; i < subsetCount; i++) {
-        const binaryString = i.toString(2).padStart(nums.length, "0")
-        subset = []
-        for (let j = 0; j < binaryString.length; j++) {
-            if (binaryString[j] === '1') {
-                subset.push(nums[j]) 
-            }
-        } results.push(subset)
+    let result = [];
+    dfs([], 0);
+    
+    function dfs(current, index){
+        result.push(current);
+        for(let i = index; i < nums.length; i++) {
+            dfs(current.concat(nums[i]), i + 1);
+        }
     }
-    return results
+    
+    return result;
 };
