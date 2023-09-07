c98455a912a09a027723b0f4f15adecff1eda3b2
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-09-06 23:29:16 -0500

    Time: 53 ms (74.96%), Space: 44 MB (65.93%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -3,11 +3,18 @@
  * @return {number}
  */
 var findNumbers = function(nums) {
-    let result = 0
-    nums.forEach(el => {
-        if (el.toString().length%2===0) {
-            result++
-        }
-    })
-    return result
+    let count = 0
+    // nums.forEach(el => {
+    //     if (el.toString().length%2===0) {
+    //         count++
+    //     }
+    // })
+    
+    for (i = 0; i < nums.length; i++) {
+        let numberOfDigits = nums[i].toString().length;
+        if (numberOfDigits%2===0) {
+            count++
+        };
+    };
+    return count
 };
