1c06d3d7efa1366c43585d0ffe4deb33eb3f82ee
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-09-14 17:29:51 -0500

    Time: 72 ms (63.72%), Space: 51.6 MB (57.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,15 +3,17 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let left = 0
-    let right = 1
-    let maxProfit = 0
+    let left = 0;
+    let right = 1;
+    let maxProfit = 0;
     while (right < prices.length) {
-        if (prices[left] < prices[right]) {
-            let profit = prices[right]-prices[left]
+        if (prices[left] < prices [right]) {
+            let profit = prices[right] - prices[left]
             maxProfit = Math.max(maxProfit, profit)
         } else {
-            left=right
-        } right++
-    } return maxProfit
+            left = right;
+        }
+        right++;
+    }
+    return maxProfit;
 };
