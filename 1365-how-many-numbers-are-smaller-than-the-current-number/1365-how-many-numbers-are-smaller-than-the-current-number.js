de30b181879c43c0362aabff4a3f7d8af427a95c
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-09-14 16:40:05 -0500

    Time: 64 ms (78.44%), Space: 44 MB (77.43%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,6 +3,7 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
-    let sorted = [...nums].sort((a, b) => a - b)
-    return nums.map((e) => sorted.indexOf(e))
+    let sorted = [...nums].sort((a, b) => a - b) // use spread operator and sort the new array
+                                                // from smallest largest
+    return nums.map((num) => sorted.indexOf(num)) // maps a new arr with the first index that the num in the original array occurs at which is the same thing as how many numbers are smaller than it.
 };
