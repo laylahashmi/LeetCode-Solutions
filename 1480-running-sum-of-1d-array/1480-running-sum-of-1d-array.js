26409f222a0af32b398230965381cab248aff36a
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-09-14 16:34:23 -0500

    Time: 49 ms (82.75%), Space: 42.9 MB (14.50%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -3,6 +3,8 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    let sum = 0;
-    return nums.map((n) => sum+=n)
+    // let sum = 0;
+    // return nums.map((n) => sum+=n)
+    return nums.map((curr, index, arr) => index > 0? arr[index] += arr[index-1] : arr[index])
+    ;
 };
