d0f72079d51acd0549e85971e92f6a9efe6bdb37
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-10 22:59:02 -0500

    Time: 473 ms (66.98%), Space: 30.9 MB (75.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,14 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        s = set()
-        # loop through my numbers
-        for n in nums:
-            # compare the numbers to the set
-            # if the number is already in the set- its a duplicate- return true
-            if n in s:
+        """
+        1. make an empty dict
+        2. loop through my numbers array
+            1. number is in the dict, return true
+            2. number is not in the dict, add it to the dict with a vaue of 1
+        3. no duplicates, answer will be false
+        """
+        
+        res = set()
+        
+        for i in range(len(nums)):
+            if nums[i] in res:
                 return True
-            # if its not in the set, we'll add it to the set
-            s.add(n)
-        #return false
+            res.add(nums[i])
         return False
+        
         
