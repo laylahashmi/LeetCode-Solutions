8cb8e1039d2aa9bb88c51ff0b21809c13102e94f
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-11 00:01:16 -0500

    Time: 54 ms (59.82%), Space: 16.7 MB (94.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -16,9 +16,5 @@ class Solution:
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
             
-        for letter in countS:
-            if countS[letter] != countT.get(letter, 0):
-                return False
-            
-        return True
+        return countS == countT
         
