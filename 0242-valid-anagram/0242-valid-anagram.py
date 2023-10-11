01886eb0e54423baf9a700b506c525f0584c75c4
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-11 00:07:30 -0500

    Time: 61 ms (23.37%), Space: 16.7 MB (94.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,9 +7,8 @@ class Solution:
         3. compare the two hashmaps and make sure each key, value pair is the same
         '''
         
-        countS, countT = {}, {}
-        
         if len(s) == len(t):
+            countS, countT = {}, {}
             for i in range(len(s)):
                 countS[s[i]] = 1 + countS.get(s[i], 0)
                 countT[t[i]] = 1 + countT.get(t[i], 0)
