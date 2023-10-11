d8e7869da92e5334449e7a90324ac5d728f7539d
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-11 14:56:36 -0500

    Time: 63 ms (73.14%), Space: 17.6 MB (43.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,11 +2,13 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
         1. create an empty hashmap
-        2. loop through my nums array and create key and value pairs
-            - find the complement (target - number at a certain index)
-            - check and see if the complement already exists in the hashmap
-                - return [number at that index and the complement]
-            - add the complement to the hashmap
+        2. loop through my numbers using the index of number
+            - subtract the number at the idx from the target and store it in a variable
+            - check if that variable is already in the hashmap
+                - return our current idx and the value of the variable in the hashmap
+                - if not in hashmap
+                    - add the number and the idx to the hashmap
+        
         """
         
         hashmap = {}
@@ -16,5 +18,4 @@ class Solution:
             if complement in hashmap:
                 return [i, hashmap[complement]]
             hashmap[nums[i]] = i
-            
         
