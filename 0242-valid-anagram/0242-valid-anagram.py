7e4c2f0ffc3650d79268430ae7624a38400815b4
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-11 14:43:53 -0500

    Time: 62 ms (19.81%), Space: 16.9 MB (49.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,21 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        """
+        1. check if length of strings are equal
+            - create two empty hashmaps for each string
+            - loop through the strings
+            - add the letter at each idx as the key and the occurence of each letter as the value
+            - if the two hashmaps equal each other, return true
+        2. Return false
+        """
         
+        countS, countT = {}, {}
+        
+        if len(s) == len(t):
+            for i in range(len(s)):
+                countS[s[i]] = countS.get(s[i], 0) + 1
+                countT[t[i]] = countT.get(t[i], 0) + 1
+                
+            return countS == countT
+        
+        return False
