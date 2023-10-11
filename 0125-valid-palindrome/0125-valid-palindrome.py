6f5f38b505c434fcbbee850227507da3c84b2c21
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-11 16:32:17 -0500

    Time: 48 ms (68.15%), Space: 17.7 MB (30.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,27 +1,29 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        """
-        1. create a function check if the chars are alphanumeric
-        2. intiate a left pointer and a right pointer
-        3. at each increment/decrement of the pointers, check to see if the char is alphanumeric and if they're equal
-            - not equal return false
-            - not alphanumeric, increment/decrement the pointers
-        """
+#         """
+#         1. create a function check if the chars are alphanumeric
+#         2. intiate a left pointer and a right pointer
+#         3. at each increment/decrement of the pointers, check to see if the char is alphanumeric and if they're equal
+#             - not equal return false
+#             - not alphanumeric, increment/decrement the pointers
+#         """
         
-        l, r = 0, (len(s) - 1)
+#         l, r = 0, (len(s) - 1)
         
-        while l < r:
-            while l < r and not self.isAlphaNum(s[l]):
-                l += 1
-            while r > l and not self.isAlphaNum(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l, r = l + 1, r - 1
+#         while l < r:
+#             while l < r and not self.isAlphaNum(s[l]):
+#                 l += 1
+#             while r > l and not self.isAlphaNum(s[r]):
+#                 r -= 1
+#             if s[l].lower() != s[r].lower():
+#                 return False
+#             l, r = l + 1, r - 1
             
-        return True
+#         return True
         
-    def isAlphaNum(self, c):
-        return  ord("A") <= ord(c) <= ord("Z") or ord("a") <= ord(c) <= ord("z") or ord("0") <= ord(c) <= ord("9")
+#     def isAlphaNum(self, c):
+#         return  ord("A") <= ord(c) <= ord("Z") or ord("a") <= ord(c) <= ord("z") or ord("0") <= ord(c) <= ord("9")
+        cleanStr = [i for i in s.lower() if i.isalnum()]
+        return cleanStr == cleanStr[::-1]
         
         
