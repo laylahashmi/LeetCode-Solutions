a7015064c2ed4e870275e9fd934f355a9f3908c1
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-11 16:30:39 -0500

    Time: 76 ms (5.92%), Space: 17.1 MB (54.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,10 +1,27 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         """
-        1. use regex to clean string
-        2. convert to lowercase only
-        3. reverse string
-        4. compare the clean string and the reversed string
+        1. create a function check if the chars are alphanumeric
+        2. intiate a left pointer and a right pointer
+        3. at each increment/decrement of the pointers, check to see if the char is alphanumeric and if they're equal
+            - not equal return false
+            - not alphanumeric, increment/decrement the pointers
         """
-        cleanStr = [i for i in s.lower() if i.isalnum()]
-        return cleanStr == cleanStr[::-1]
+        
+        l, r = 0, (len(s) - 1)
+        
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):
+                l += 1
+            while r > l and not self.isAlphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+            
+        return True
+        
+    def isAlphaNum(self, c):
+        return  ord("A") <= ord(c) <= ord("Z") or ord("a") <= ord(c) <= ord("z") or ord("0") <= ord(c) <= ord("9")
+        
+        
