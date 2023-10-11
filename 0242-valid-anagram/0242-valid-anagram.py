ca6352ecd66ea7bd93b803ebca919a779294ea52
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-10 23:55:30 -0500

    Time: 62 ms (19.85%), Space: 16.7 MB (94.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,24 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        '''
+        1. compare the lengths of the two str
+        2. create hashmaps for each string and create key, value pairs 
+            -key will be the letter in the strings and value will be the occurence of the letter in the strings
+        3. compare the two hashmaps and make sure each key, value pair is the same
+        '''
+        
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+            
+        for letter in countS:
+            if countS[letter] != countT.get(letter, 0):
+                return False
+            
+        return True
         
