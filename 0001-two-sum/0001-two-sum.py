350db8ea7324a0c848ac561526e413c9bbdf82ba
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-11 12:15:19 -0500

    Time: 61 ms (81.39%), Space: 17.5 MB (43.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,15 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         """
-        1. outer loop will traverse the list start at the first position
-        2. inner loop will traverse starting at the second position
-        3. add the numbers at the two indices to see if they equal the target
-        4. if they do, we'll push the two numbers to our result array
-        5. if they don't, we'll move to the next indices
+        1. create an empty hashmap
+        2. loop through my nums array and create key and value pairs
+            - find the complement (target - number at a certain index)
+            - check and see if the complement already exists in the hashmap
+                - return [number at that index and the complement]
+            - add the complement to the hashmap
         """
         
+        hashmap = {}
+        
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+            
         
