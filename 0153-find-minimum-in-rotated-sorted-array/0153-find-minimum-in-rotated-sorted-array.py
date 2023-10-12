7143c23cd4f104481ee9f309f5f767b3fb209a1c
laylahashmi <129443002+laylahashmi@users.noreply.github.com>
2023-10-11 22:25:46 -0500

    Time: 38 ms (96.18%), Space: 16.7 MB (31.69%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,3 +1,35 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        return min(nums)
+        """
+        1. res which holds our min value
+        2. left and right pointers
+        3. while left is less than right
+            1. if value at left is less than value at right
+                -min value will be the min of the res and the value at the left
+                - break
+            2. find the middle value of the arr
+                - set res to min of either res or to the middle value
+                - determine if we want to move to the left or to the right of the middle value
+                    - if the middle value is greater than or equal to the value at left pointer
+                        - it is part of the left portion and we check the right portion for the min value
+                    - otherwise we want to move to the left portion and check for the min value there
+        4. return the result
+            
+        """
+        
+        res = nums[0]
+        l, r = 0, len(nums)-1
+        
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]: 
+                l = m + 1
+            else:
+                r = m - 1
+        return res
+            
